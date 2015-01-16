5bee973dd0c1ba2ba5b473398429c2f4c5f6447e
Wig C <onlywig@gmail.com>
2015-01-16 08:55:58 +0800

    Add date and storage percent functions.

--- main.c
+++ main.c
@@ -1,10 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 
 int main(int argc,char* argv)
 {
+	FILE *ps;
+	char date_str[32]={0};
 
+	if((ps = popen("date +%Y%m%d-%H%M%S", "r")) == NULL )
+	{
+		printf("popen() error!\n");
+		return 1;
+	}
 
+	fgets(date_str, sizeof(date_str),ps);
+	printf("%s\n",date_str);
+
+	pclose(ps);
+
+	if((ps = popen("df -h | grep rootfs | awk '{print $5}'", "r")) == NULL )
+	{
+		printf("popen() error!\n");
+		return 1;
+	}
+
+	memset(date_str,0,sizeof(date_str));
+	fgets(date_str, sizeof(date_str),ps);
+	printf("%s\n",date_str);
+
+	pclose(ps);
 
 	return 0;
 }
